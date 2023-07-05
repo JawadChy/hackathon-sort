4822406393ae259904f8f08f1481ce8a2f09f9a3
Jawad Chowdhury <124181330+JawadChy@users.noreply.github.com>
2023-07-05 09:05:12 -0400

    user can now work with default list or new list

--- sort.py
+++ sort.py
@@ -24,8 +24,10 @@ hackathon4 = Hackathon("Alex's Birthday Hackathon", date(2024, 1, 1))
 hackathon5 = Hackathon("Pancake Hacks", date(2023, 9, 26))
 hackathon6 = Hackathon("ChrisTM Hackathon", date(2023, 5, 31))
 
-# List of hackathons
-hackathons = [hackathon1, hackathon2, hackathon3, hackathon4, hackathon5]
+# Default list of hackathons
+defaultHackathons = [hackathon1, hackathon2, hackathon3, hackathon4, hackathon5]
+newHackathons = []
+
 
 print("How would you like to sort the hackathons? Alphabetically or by date?")
 print("Enter '1' for alphabetical or '2' for date : ")
@@ -34,7 +36,45 @@ userInput = input()
 
 while userInput != "1" and userInput != "2":
     print("Unrecognized input. Please enter '1' for alphabetical or '2' for date : ")
-    userInput =input()
+    userInput = input()
+
+print()
+print("type '1' if you want to work with the default hackathon list, and type '2' if you want to work with a new list : ")
+
+userInput2 = input()
+
+while userInput2 != "1" and userInput != "2":
+    print("Unrecognized input. Please enter '1' for default list sorting or '2' for new list sorting : ")
+    userInput2 = input()
+
+if userInput2 == "1":
+    hackathons = defaultHackathons
+else:
+    hackathons = newHackathons
+
+addMoreHackathons = False
+
+print()
+print("Type 'y' if you want to add more hackathons to the current list, or type 'n' to continue to sorting : ")
+userInput3 = input()
+
+while userInput3 != "y" and userInput != "n":
+    print("Unrecognized input. Please enter 'y' to add a value to the list, or type 'n' to continue with adding a new value : ")
+    userInput3 = input()
+    if userInput3 == "y":
+        # Taking in values of new hackathon to add
+        hackathonName = input("Enter the name of the hackathon to add : ")
+        hackathonYear = int(input("Enter the year the hackathon occurs : "))
+        hackathonMonth = int(input("Enter the month the hackathon occurs : "))
+        hackathonDay = int(input("Enter the day the hackathon occurs : "))
+        hackathonDate = date(hackathonYear, hackathonMonth, hackathonDay)
+        # Make a hackathon object with this given information
+        hackathonToAdd = Hackathon(hackathonName, hackathonDate)
+        # Actually append to hackathons list
+        hackathons.append(hackathonToAdd)
+        print()
+        print("Hackathon added to list.")
+        print("Type 'y' if you want to add more hackathons to the current list, or type 'n' to continue to sorting : ")
 
 if userInput == "1":
     sortAlphabetically(hackathons)
